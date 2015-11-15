3f7a295cd1e9633f20366ec8e27ec04502e3cced
Yuqi Hou <yuqi.hou@gmail.com>
2015-11-14 20:04:17 -0500

    Moved

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
-from flask import Flask, request, redirect
-import twilio.twiml
- 
+from flask import Flask
+from flask import request
+from flask import render_template
+import os
+from random import choice
+
+
 app = Flask(__name__)
- 
+
 # Try adding your own number to this list!
 callers = {
-    "+14244420724": "Yuqi GV",
-    "+19133784671": "Yuqi Cell"
+    "+19133784671": "Yuqi"
 }
- 
-@app.route("/", methods=['GET', 'POST'])
-def hello_world():
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/sms", methods=['GET', 'POST'])
+def hello_word():
     """Respond and greet the caller by name."""
  
     from_number = request.values.get('From', None)
     if from_number in callers:
         message = callers[from_number] + ", thanks for the message!"
     else:
-        message = "Hey you, thanks for the message!"
+        message = "Monkey, thanks for the message!"
  
     resp = twilio.twiml.Response()
     resp.message(message)
  
     return str(resp)
  
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+
+    if port == 5000:
+        app.debug = True
+
+    app.run(host='0.0.0.0', port=port)
